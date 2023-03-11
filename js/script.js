c639472ec757fe0fa3bc6c90b15f8bae98e03dc6
sestradarya <nguendarina1@gmail.com>
2023-03-11 14:34:10 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -1,121 +1,112 @@
-const inputEl = document.getElementById("main-input")
-const addBtn = document.getElementById("add-button")
-const tasksEl = document.getElementById("tasks")
-const clearBtn = document.getElementById("clear-button")
+const inputEl = document.getElementById("main-input");
+const addBtn = document.getElementById("add-button");
+const tasksEl = document.getElementById("tasks");
+const clearBtn = document.getElementById("clear-button");
 
+function addTask() {
+  const taskText = inputEl.value;
+  const id = getRandomId();
+  const taskObj = {
+    id: id,
+    text: taskText,
+    isChecked: false,
+  };
 
-async function addTask() {
-    const taskText = inputEl.value;
-    const id = await getRandomId();
-    const taskObj = {
-        id: id,
-        text: taskText,
-        isChecked: false
-    }
-
-    if (taskText) {
-        addToLS(taskObj);
-        renderTasks();
-
-    }
-    inputEl.value = ''
-
+  if (taskText) {
+    addToLS(taskObj);
+    renderTasks();
+  }
+  inputEl.value = "";
 }
 
-tasksEl.addEventListener('click', (event) => {
-    const targetEl = event.target
-    const parentEl = targetEl.parentElement
-    const id = parentEl.getAttribute('taskid')
+tasksEl.addEventListener("click", (event) => {
+  const targetEl = event.target;
+  const parentEl = targetEl.parentElement;
+  const id = parentEl.getAttribute("taskid");
 
-    if (targetEl.className === 'checkbox') {
-        if (targetEl.checked) {
-            changeCheckedInLS(id)
-            renderTasks()
-        }
-        else {
-            changeCheckedInLS(id)
-            renderTasks()
-        }
-    }
-    if (targetEl.className === 'delete-button') {
-        removeFromLS(id);
-        renderTasks()
-    }
-
-})
-
+  if (targetEl.className === "checkbox") {
+    changeCheckedInLS(id)
+    renderTasks()
+  }
+  if (targetEl.className === "delete-button") {
+    console.log('del')
+    removeFromLS(id);
+    renderTasks();
+  }
+});
 
 function renderTasks() {
-    tasksEl.innerHTML = ''
+  tasksEl.innerHTML = "";
 
+  let list = getFromLS();
 
-    let list = getFromLS()
+  if (list === null) {
+    tasksEl.innerHTML = "<hr>";
+    localStorage.setItem("taskList", JSON.stringify([]));
+  }
 
-
-    if (list === null) { tasksEl.innerHTML = '<hr>'; localStorage.setItem('taskList', JSON.stringify([])); }
-
-    list = getFromLS();
-    for (obj of list) {
-        tasksEl.innerHTML += `
+  list = getFromLS();
+  if (list) {
+    for (let obj of list) {
+      tasksEl.innerHTML += `
             <div class="task" taskid = "${obj.id}" >
                 <button class="delete-button">
                     <i class="fa-sharp fa-solid fa-xmark"></i>
                 </button>
-                <input class="checkbox" type="checkbox" ${obj.isChecked && 'checked'}>
-                <p class="task-text" style="${obj.isChecked && "text-decoration: line-through; color: #DE6262;"}">${obj.text}</p>
+                <input class="checkbox" type="checkbox" ${
+                  obj.isChecked && "checked"
+                }>
+                <p class="task-text" style="${
+                  obj.isChecked &&
+                  "text-decoration: line-through; color: #DE6262;"
+                }">${obj.text}</p>
             </div>
             <hr>
-        `
+        `;
     }
+  }
 }
 
-
 function addToLS(task) {
-    const list = getFromLS()
-    localStorage.setItem('taskList', JSON.stringify([...list, task]))
+  const list = getFromLS();
+  localStorage.setItem("taskList", JSON.stringify([...list, task]));
 }
 
 function getFromLS() {
-    return JSON.parse(localStorage.getItem('taskList'))
+  return JSON.parse(localStorage.getItem("taskList"));
 }
 
 function changeCheckedInLS(id) {
-    const list = getFromLS()
-    const newList = []
-    for (let el of list) {
-        el.id !== id ?
-            newList.push(el) :
-            newList.push({ ...el, isChecked: !el.isChecked })
-    }
-    localStorage.setItem('taskList', JSON.stringify(newList))
+  const list = getFromLS();
+  const newList = [];
+  for (let el of list) {
+    el.id != id
+      ? newList.push(el)
+      : newList.push({ ...el, isChecked: !el.isChecked });
+  }
+  localStorage.setItem("taskList", JSON.stringify(newList));
 }
 
 function removeFromLS(taskId) {
-    const list = getFromLS()
-    const newList = []
-    for (let el of list) {
-        el.id !== taskId && newList.push(el)
-    }
-    localStorage.setItem('taskList', JSON.stringify(newList))
+  const list = getFromLS();
+  const newList = [];
+  for (let el of list) {
+    el.id != taskId && newList.push(el);
+  }
+  localStorage.setItem("taskList", JSON.stringify(newList));
 }
 
-async function getRandomId() {
-    const res = await fetch('https://www.uuidtools.com/api/generate/v1');
-    const data = await res.json();
-    return data[0]
+function getRandomId() {
+  return Math.floor(Math.random() * 10000);
 }
 
-addBtn.addEventListener('click', () => {
-    addTask()
-})
+addBtn.addEventListener("click", () => {
+  addTask();
+});
 
-clearBtn.addEventListener('click', () => {
-    tasksEl.innerHTML = `<hr>`
-    localStorage.setItem('taskList', JSON.stringify([]))
-})
+clearBtn.addEventListener("click", () => {
+  tasksEl.innerHTML = `<hr>`;
+  localStorage.setItem("taskList", JSON.stringify([]));
+});
 
-renderTasks()
-
-
-// bugs:
-// pressing a delete button working not only on the whole button
+renderTasks();
